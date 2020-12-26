f0a2d50111e439eee68d02cfa334c7df50c746ea
Vieira <diego.vieira@accenture.com>
2020-12-26 11:42:25 -0300

    Primeira versão do Microsserviço

---
+++ src/main/java/br/com/dsv/music/streaming/custom/api/application/Application.java
@@ -0,0 +1,13 @@
+package br.com.dsv.music.streaming.custom.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
